0a02852c7c583d542db8526fea67e260860f825f
NabarunKar <nabarunkar01@gmail.com>
2023-05-12 19:17:58 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from "@material-ui/icons";
+import { Badge, MenuItem } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -16,24 +17,25 @@ const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
+  
 `;
 
 const Language = styled.span`
-    font-size: 14px;
-    cursor: pointer;
+  font-size: 14px;
+  cursor: pointer;
 `;
 
 const SearchContainer = styled.div`
-    border: 0.5px solid lightgray;
-    display: flex;
-    align-items: center;
-    margin-left: 25px;
-    padding: 5px;
-`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
 
 const Input = styled.input`
-    border: none;
-`
+  border: none;
+`;
 
 const Center = styled.div`
   flex: 1;
@@ -41,11 +43,20 @@ const Center = styled.div`
 `;
 
 const Logo = styled.h1`
-    font-weight: bold;
-`
+  font-weight: bold;
+`;
 
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const menuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -55,12 +66,22 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input/>
-            <Search/>
+            <Input />
+            <Search style = {{color:"gray", fontSize:16}}/>
           </SearchContainer>
         </Left>
-        <Center><Logo>MART.</Logo></Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>MART.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
