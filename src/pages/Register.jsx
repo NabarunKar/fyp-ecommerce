ac0a75b09e691c125c54af661f86f0246a3b8183
NabarunKar <nabarunkar01@gmail.com>
2023-05-13 19:10:55 +0530

    finished design

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-// import { mobile } from "../responsive";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   /* since it is a full screen component */
@@ -21,6 +21,7 @@ const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
   background-color: white;
+  ${mobile({ width: "75%" })}
 `;
 
 const Title = styled.h1`
